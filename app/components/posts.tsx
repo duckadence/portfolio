a859289cde7c0b84d62ba30147ff2c2ba2c07105
duckadence <70464132+duckadence@users.noreply.github.com>
2025-04-15 12:29:07 -0700

    fixed blog title align and general spacing issues

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,11 +21,14 @@ export function BlogPosts() {
             className="flex flex-col space-y-1 mb-8"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full grid grid-rows-2 grid-flow-row gap-2">
-              <div><p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+            <div className="w-full grid grid-flow-col grid-rows-2 space-x-6">
+			  <div class="row-span-2"><p className="text-neutral-500 dark:text-neutral-400 w-[69px] tabular-nums">
+                {formatDate(post.metadata.publishedAt, false)}
+              </p></div>
+              <div class="text-left"><p className="text-neutral-900 dark:text-neutral-100 w-screen tracking-tight">
                 {post.metadata.title}
               </p></div>
-			  <div class="flex flex-row justify-start my">
+			  <div class="flex flex-row justify-start h-fit">
 				<p className="empty:hidden text-neutral-900 dark:text-neutral-100 tracking-tight bg-neutral-400 dark:bg-neutral-700 rounded-md mr-1 pb-0.5 px-1">
                 {post.metadata.tag1}
 				</p>
@@ -41,11 +44,8 @@ export function BlogPosts() {
 				<p className="empty:hidden text-neutral-900 dark:text-neutral-100 tracking-tight bg-neutral-400 dark:bg-neutral-700 rounded-md mr-1 pb-0.5 px-1">
                 {post.metadata.tag5}
 				</p>
-		</div>
-			  <div class="text-left row-span-2"><p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p></div>
-            </div>
+			  </div>
+			</div>
           </Link>
         ))}
     </div>
