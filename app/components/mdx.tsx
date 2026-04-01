10d38c0b0ec518ac79d5b686bc52486622c2637d
duckadence <70464132+duckadence@users.noreply.github.com>
2026-03-31 19:04:33 -0700

    Update mdx.tsx

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { MDXRemote } from 'next-mdx-remote/rsc'
+import { MDXRemote } from 'next-mdx-remote'
 import { highlight } from 'sugar-high'
 import { YouTubeComponent } from "./youtube"
 
