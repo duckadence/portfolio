c3e82d76774faedbd8795a0f6c90502038a91060
duckadence <70464132+duckadence@users.noreply.github.com>
2026-04-26 18:27:54 -0700

    Update home page blurb

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import { BlogPosts } from "app/components/posts";
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Hi!</h1>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Hi, I'm Jonathan
+      </h1>
       <p className="mb-4">
-        {`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`}
+        {`Welcome to my site!  I make stuff using microcontrollers, 3d printing, and electronics.`}
       </p>
       <div className="my-8">
         <BlogPosts limit={3} showViewAll />
